f9ecb1692431387ae1b2c28e79643dd50bd19a5b
ronald1101 <dsouzaronald13@gmail.com>
2019-05-28 13:00:32 +0400

    hide search on details

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -3,11 +3,37 @@ import './Navbar.scss';
 import menuIcon from '../../assets/icons/white-menu.jpg';
 import { connect } from 'react-redux';
 import { SEARCH_TEXT } from '../../store/actions';
+import { withRouter } from 'react-router-dom';
 
 class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showSearch: true
+        }
+    }
+
+    searchInputHandler = (location) => {
+        if (location.pathname !== '/') {
+            this.setState({
+                showSearch: false
+            })
+        }
+        else {
+            this.setState({
+                showSearch: true
+            })
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // only update chart if the location has changed
+        if (prevProps.location !== this.props.location) {
+            this.searchInputHandler(this.props.location);
+        }
+    }
 
     render() {
-
         return (
             <nav className="navbar aqua-marine" >
                 <div className="navbar-brand white">
@@ -16,11 +42,13 @@ class Navbar extends Component {
                             <img src={menuIcon} width="20" height="20" className="d-inline-block align" alt="" />
                             <span className="nav-title pad-left">NY Times Most Popular</span>
                         </div>
-                        <div className="col-6">
-                            <div className="float-right">
-                                <input type="text" className="form-control" placeholder="Search" onChange={this.props.onChange.bind(this)} />
-                            </div>
-                        </div>
+                        {
+                            this.state.showSearch ? <div className="col-6">
+                                <div className="float-right">
+                                    <input type="text" className="form-control" placeholder="Search" onChange={this.props.onChange.bind(this)} />
+                                </div>
+                            </div> : null
+                        }
                     </div>
                 </div>
             </nav >
@@ -39,4 +67,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(null, mapDispatchToProps)(Navbar);
+export default connect(null, mapDispatchToProps)(withRouter(Navbar));
